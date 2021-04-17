616081ea788eda1b3d67d1c8e5c62bba0312c300
Max Stickels <maxwellstickels@gmail.com>
2021-04-17 13:58:36 -0700

    Changed to MLB for stats obtained

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+var nameSearch = document.getElementById("name-search");
+var submitBtn = document.getElementById("submit-btn");
+var info;
+var info2;
 
-fetch("https://www.balldontlie.io/api/v1/games/").then(function(response) { 
-    console.log(response.status);
-    return response.json();
-    }).then(function(data) {
-    console.log(data);
+function displayResults() {
+    fetch("http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw='Y'&name_part='" + nameSearch.value + "%25'").then(function(response) { return response.json();}).then(function(data) {
+        if (data.search_player_all.queryResults.totalSize != "0") {
+                info = data.search_player_all.queryResults.row;
+                for (var i = 0; i < info.length; i++) {
+                    var nickName = " \"";
+                    if (info[i].name_nick === "") {
+                        nickName = " ";
+                    }
+                    else {
+                        nickName += info[i].name_nick + "\" ";
+                    }
+                    
+                    var jerseyNum = info[i].jersey_number;
+                    if (jerseyNum) {
+                        jerseyNum += "00";
+                    }
+                    console.log("#" + jerseyNum + ": " + info[i].name_first + nickName + info[i].name_last);
+                }
+            return data;
+        }
+    });
+}
 
-    return data;
-});
+submitBtn.addEventListener("click", displayResults);
